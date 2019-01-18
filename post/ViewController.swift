458f9cecdf3c1dd241702e1d98f9c6ce48e1e51d
mgt <mgt@Mac-mini-2.local>
2019-01-18 14:21:40 +0900

    Initial Commit

---
+++ post/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  post
+//
+//  Created by mgt on 2019/01/18.
+//  Copyright © 2019 mgt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
